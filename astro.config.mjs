3890ad96b465278b5d293ac97cf2d94cb2a1fc56
Felipe Ferreira <feferreira.sf@gmail.com>
2025-04-20 20:24:50 -0300

    🚀

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()]
-	}
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [react(), sitemap()],
+  site: "https://mentoria.kamilasantos.dev", // todo: colocar o link do site
+  output: "server",
+  adapter: vercel(),
 });
