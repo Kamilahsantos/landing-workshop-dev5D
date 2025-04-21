9d8edf9337407e1d943953c7c566999ed50830de
Felipe Ferreira <feferreira.sf@gmail.com>
2025-04-21 19:20:40 -0300

    ✨ Enhance MainButton component to handle external links with appropriate target and rel attributes

--- src/components/ui/MainButton.tsx
+++ src/components/ui/MainButton.tsx
@@ -9,6 +9,7 @@ type MainButtonProps = {
 };
 
 export default function MainButton({ children, href, badge }: MainButtonProps) {
+  const isExternal = href && href.startsWith("http");
   const commonClasses =
     "w-full h-14 cursor-pointer transition-transform duration-300 ease-in-out bg-emerald-500 hover:bg-emerald-700 hover:scale-105 focus:ring-2 focus:ring-emerald-400 focus:ring-offset-2 relative";
 
@@ -18,6 +19,8 @@ export default function MainButton({ children, href, badge }: MainButtonProps) {
         href={href}
         className={commonClasses + " flex items-center justify-center rounded-md no-underline gap-2"}
         role="button"
+        target={isExternal ? "_blank" : undefined}
+        rel={isExternal ? "noreferrer" : undefined}
       >
         {badge && (
           <span className="absolute top-0 right-0 -mt-2 -mr-2 bg-purple-600 text-white text-xs font-bold px-2 py-0.5 rounded-full shadow-md">
@@ -25,7 +28,7 @@ export default function MainButton({ children, href, badge }: MainButtonProps) {
           </span>
         )}
         {children}
-        <ExternalLink />
+        {isExternal && <ExternalLink />}
       </a>
     );
   }
