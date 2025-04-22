1e331a3526a10ef1be20d78acefa37799a6fc334
Kamila Santos <kahsantos89@gmail.com>
2025-04-22 01:21:23 -0300

    copy finalizada

--- src/components/ui/FaqAccordion.tsx
+++ src/components/ui/FaqAccordion.tsx
@@ -4,52 +4,45 @@ import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion";
+} from '@/components/ui/accordion';
 
 const faqs = [
   {
     question: 'Este workshop é para qual nível de experiência?',
-    answer: 'Todos níveis de experiência são bem-vindos!',
+    answer:
+      'Para desenvolvedores que já atuam na área no mínimo como desenvolvedor júnior e que já tenham experiência em desenvolvimento',
   },
   {
-    question: 'O que é o Frontend Mentor?',
-    answer: 'O Frontend Mentor é uma comunidade de desenvolvedores que compartilham suas experiências e conhecimentos para ajudar os iniciantes a crescer e alcançar seus objetivos.',
+    question: 'E se eu não puder estar presente no workshop?',
+    answer:
+      'Você terá como assistir a gravação se estiver incluso no seu tipo de ingresso. Porém a gravação da imersão só será disponibilizada durante 60 dias. Por isso, é de extrema importância que esteja ao vivo..',
   },
   {
-    question: 'Como eu posso participar desse workshop?',
-    answer: 'Você pode participar desse workshop de forma gratuita ou pagar uma taxa mensal. A taxa paga é de R$ 99,99 mensais.',
+    question: 'Quando e onde será a imersão?',
+    answer:
+      'Acontecerá ao vivo, online em uma sala exclusiva no Meet ao qual enviaremos o link de acesso para todos os inscritos, dia 10 de maio , sábado das 10h às 18h no horário de Brasília.',
   },
   {
-    question: 'Qual o prazo para participar desse workshop?',
-    answer: 'O prazo para participar desse workshop é de até o dia 19 de agosto de 2022.',
+    question: 'Vai ficar gravado?',
+    answer:
+      'Apenas se você comprar o ingresso VIP - Dev estratégico, assim você terá acesso a 60 dias da gravação.',
   },
   {
-    question: 'Qual o horário para participar desse workshop?',
-    answer: 'O horário para participar desse workshop é de das 10h às 18h.',
-  },
-  {
-    question: 'Qual a diferença entre o Frontend Mentor e o Frontend Master?',
-    answer: 'O Frontend Master é um curso de formação completa para desenvolvedores Frontend, enquanto o Frontend Mentor é uma comunidade de desenvolvedores que compartilham suas experiências e conhecimentos para ajudar os iniciantes a crescer e alcançar seus objetivos.',
+    question: 'Como posso garantir meu ingresso então?',
+    answer:
+      'Você irá clicar em um dos botões na nossa página de “Quero o ingresso” que você será direcionado a uma página segura de pagamento e pode comprar seu ingresso. Estou te esperando do outro lado, animada para acelerar sua carreira!',
   },
 ];
 
 export default function FaqAccordion() {
   return (
-    <Accordion
-      type="single"
-      collapsible
-      className="w-full max-w-4xl mx-auto text-left"
-    >
-      {
-        faqs.map(({ question, answer }, index) => (
-          <AccordionItem value={`accordion-question-${index}`} key={index}>
-            <AccordionTrigger>{question}</AccordionTrigger>
-            <AccordionContent>
-              {answer}
-            </AccordionContent>
-          </AccordionItem>
-        ))
-      }
+    <Accordion type='single' collapsible className='w-full max-w-4xl mx-auto text-left'>
+      {faqs.map(({ question, answer }, index) => (
+        <AccordionItem value={`accordion-question-${index}`} key={index}>
+          <AccordionTrigger>{question}</AccordionTrigger>
+          <AccordionContent>{answer}</AccordionContent>
+        </AccordionItem>
+      ))}
     </Accordion>
   );
 }
