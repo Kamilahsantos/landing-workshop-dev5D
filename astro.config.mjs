d7fa265b69c05c071eb2b5ed7aa2b0caf5bac048
Kamila Santos <kahsantos89@gmail.com>
2025-04-22 01:45:35 -0300

    seo adjusts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel";
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,12 +12,12 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   integrations: [react(), sitemap()],
-  site: "https://mentoria.kamilasantos.dev", // todo: colocar o link do site
-  output: "server",
+  site: 'https://workshop-dev-5d.vercel.app/', // todo: colocar o link do site
+  output: 'server',
   adapter: vercel({
     imageService: true,
   }),
   image: {
-    remotePatterns: [{ protocol: "https", hostname: "avatars.githubusercontent.com" }],
-  }
+    remotePatterns: [{ protocol: 'https', hostname: 'avatars.githubusercontent.com' }],
+  },
 });
